6d7684a918b69d8a4a2980d0286fddd770136edc
pingdecopong <pingdecopong@gmail.com>
2013-11-10 02:08:09 +0900

    修正

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 
-default["php54"]["xdebug"]["remote_enable"] = "1"
-default["php54"]["xdebug"]["remote_host"] = "10.0.2.2"
-default["php54"]["xdebug"]["remote_port"] = "9000"
+default["php"]["xdebug"]["remote_enable"] = "1"
+default["php"]["xdebug"]["remote_host"] = "10.0.2.2"
+default["php"]["xdebug"]["remote_port"] = "9000"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,16 +6,17 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-#%w(php54 php54-cli php54-pdo php54-mbstring php54-mcrypt php54-pecl-memcache php54-mysql php54-devel php54-common php54-pgsql php54-pear php54-gd php54-xml php54-pecl-xdebug php54-pecl-apc php54-process php54-intl).each do |package|
-#  yum_package package do
-#    action :install
-#  end
-#end
-%w(php54 php54-cli php54-pdo php54-mbstring php54-mysql php54-devel php54-common php54-pecl-xdebug).each do |package|
+#delete php54-mcrypt
+%w(php54 php54-cli php54-pdo php54-mbstring php54-pecl-memcache php54-mysql php54-devel php54-common php54-pgsql php54-pear php54-gd php54-xml php54-pecl-xdebug php54-pecl-apc php54-process php54-intl).each do |package|
   yum_package package do
     action :install
   end
 end
+#%w(php54 php54-cli php54-pdo php54-mbstring php54-mysql php54-devel php54-common php54-pecl-xdebug).each do |package|
+#  yum_package package do
+#    action :install
+#  end
+#end
 
 template "php.ini" do
   path "/etc/php.ini"
@@ -23,5 +24,5 @@ template "php.ini" do
   owner "root"
   group "root"
   mode 0644
-  notifies :reload, 'service[httpd]'
+#  notifies :reload, 'service[httpd]'
 end
